e92ea96022e65368ca7c6d34c750a598c8b3c041
AnmolDhima12 <anmoldhimans525@gmail.com>
2025-06-12 23:09:57 +0530

    project completed

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const axiosToInstance =  axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1/" :"/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1/" :"/api/v1",
     withCredentials:true,
 })
